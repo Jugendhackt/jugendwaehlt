effd6c148014924369ad4bd5fc11ed63565920f2
JHGitty <jhgitty@trash-mail.com>
2013-09-08 01:01:30 +0200

    DB fixes, Vote, Where-Feature in JSON(P) Api

--- system/database.php
+++ system/database.php
@@ -2,44 +2,58 @@
 class Database {
 	private $db;
 
-	function __construct($hostname, $database, $username, $password) {
+	function __construct($hostname, $database, $username, $password, $debug=false) {
 		try {
+			// Connection
 			$this->db = new PDO('mysql:host='.$hostname.';dbname='.$database, $username, $password);
+			
+			// Error Reporting			
+			if ($debug == true) {
+				$this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			}
 		}
 		catch(PDOException $e) {
 			throw new Exception('Could not connect to database. Error: ' . $e->getMessage());
 		}
 	}
 
-	/*
-	public function login($username, $password) {
-		$sql = 'SELECT id FROM `users` WHERE `username` = ? AND `password` = ?';
-		$res = $db->prepare($sql);
-		$res->execute(array($username, $password));
-		return $res->fetchColumn();
-	}
-	*/
-
 	private function getResult($table, $where='') {
 		$where_queries = array();
+		$indexWhere = 1;
+
+		if ($table != 'Partei' && $table != 'Uservoting') {
+			echo 'Illegal table.';
+			exit();
+		}
 
 		if(!empty($where)) {
 
 			foreach($where as $k=>$v) {
-				$where_queries[] = $k.' = :'.$v;
+				$where_queries[] = $k." = ?";
 			}
 
+<<<<<<< HEAD
 			$sth = $this->db->prepare('SELECT * FROM ? WHERE ?');
 			$sth->bindValue(1, $table);
 			$sth->bindValue(2, implode(' AND ', $where_queries));
 			foreach($where as $k=>$v) {
 				 $sth->bindValue(':'.$k, $v);
+=======
+			$res = $this->db->prepare("SELECT * FROM ".$table." WHERE ".implode(' AND ', $where_queries));
+			foreach($where as $k=>$v) {
+				 $res->bindValue($indexWhere, $v);
+				 $indexWhere++;
+>>>>>>> Database fixes, Vote, Where-Feature in JSON
 			}
 		}
 		else
 		{
+<<<<<<< HEAD
 			$res = $this->db->prepare('SELECT * FROM ?');
 			$res->bindValue(1, $table);
+=======
+			$res = $this->db->prepare("SELECT * FROM ".$table);
+>>>>>>> Database fixes, Vote, Where-Feature in JSON
 		}
 
 		$res->execute();
@@ -50,7 +64,11 @@ class Database {
 		return $this->getResult($table, $where)->fetchAll(PDO::FETCH_ASSOC);
 	}
 	
+<<<<<<< HEAD
 	public function vote($partei_id,$thema_id, $why){
+=======
+	public function vote($partei_id, $thema_id, $why){
+>>>>>>> Database fixes, Vote, Where-Feature in JSON
 		$res = $this->db->prepare("INSERT INTO Uservoting(Partei_ID, Themengebiet_ID, Begruendung) VALUES(?, ?, ?)");
 		$res->bindValue(1, $partei_id);
 		$res->bindValue(2, $thema_id);
--- system/get.php
+++ system/get.php
@@ -2,21 +2,40 @@
 require_once("config.php");
 require_once("database.php");
 
-// header("Content-type: text/json");
+// Header
+header('content-type: application/json; charset=utf-8');
 
-// Prepare
-$table = strtolower($_REQUEST['t']);
+// Needed Params
+if (!isset($_REQUEST['table'])) {
+	echo "Param 'table' missed.";
+	exit();
+}
+
+// Prepare 'Table'-Feature
+$table = $_REQUEST['table'];
+
+// Prepare 'Where'-Feature
+$where = array();
+foreach($_REQUEST as $k=>$v) {
+	$where[$k] = $v;
+}
+unset($where["table"]);
+unset($where["callback"]);
+unset($where["_"]);
 
 // Get from DB
-$db = new Database(SQL_HOSTNAME, SQL_DATABASE, SQL_USERNAME, SQL_PASSWORD);
-$result = $db->fetchAll($table);
+$db = new Database(SQL_HOSTNAME, SQL_DATABASE, SQL_USERNAME, SQL_PASSWORD, true);
+$result = $db->fetchAll($table, $where);
 
 // UTF8_Encode & HTMLEntities
 array_walk_recursive($result, function (&$value) {
     $value = utf8_encode(htmlentities($value));
 });
 
-// Print JSON
-print_r(json_encode($result));
+// Print JSON(P)
+$json = json_encode($result);
+echo isset($_GET['callback'])
+    ? "{$_GET['callback']}($json)"
+    : $json;
 
 ?>
