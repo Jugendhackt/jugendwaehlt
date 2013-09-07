7dd9e36ea2ff3aa09efdf3fa0654ebedfa28c390
lukasepple <post@lukasepple.de>
2013-09-07 22:18:44 +0200

    implemented vote.php

--- system/vote.php
+++ system/vote.php
@@ -1,3 +1,16 @@
 <?php
-echo "You're Request can't be processed yet, we're implementing!"
+require_once("./config.php");
+require_once("./database.php");
+
+
+$partei_id = $_POST['partei'];
+
+$thema_id = $_POST['thema'];
+
+$begruendung = $_POST['why'];
+
+$db = new Database(SQL_HOSTNAME, SQL_DATABASE, SQL_USERNAME, SQL_PASSWORD);
+if(!($db->vote($partei_id, $thema_id, $begruendung))){
+	exit("Fail!");
+}
 ?>
