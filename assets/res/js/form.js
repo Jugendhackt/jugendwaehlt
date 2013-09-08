f8571f9552cf52ef2e515d1d81297c3066323445
Frank Blechschmidt <frank.blechschmidt@student.hpi.uni-potsdam.de>
2013-09-08 12:02:15 +0200

    change to themengebiet via id

--- assets/res/js/form.js
+++ assets/res/js/form.js
@@ -1,4 +1,5 @@
 var themen = [];
+var themengebiete = ["internet", "bildungundforschung", "umwelt", "verbraucherschutz", "kunstundkultur", "familieundjugend", "gesundheit", "aussenundsicherheitspolitik", "energie", "arbeit", "wirtschaftundfinanzen", "innneundrechtspolitik", "steuern", "bauenundverkehr"]
 
 function toggleTextbox (element, title) {
 	var themaId = $(element).attr('id');
@@ -12,7 +13,7 @@ function toggleTextbox (element, title) {
 		if ($.inArray(themaId, themen) == -1)
 		{
 			themen.push(themaId);
-			$("#themen").append("<div class=\"panel panel-info\" id=\"" + themaId + "Panel\"><div class=\"panel-heading\">" + title + "</div><div class=\"panel-body\"><input type=\"hidden\" name=\"themaID" + themen.length +"\" value=\"" + themaId + "\"><textarea class=\"form-control\" rows=\"3\" name=\"themaReason" + themen.length +"\"></textarea></div></div>");
+			$("#themen").append("<div class=\"panel panel-info\" id=\"" + themaId + "Panel\"><div class=\"panel-heading\">" + title + "</div><div class=\"panel-body\"><input type=\"hidden\" name=\"themaID" + themen.length +"\" value=\"" + (themengebiete.indexOf(themaId)+1) + "\"><textarea class=\"form-control\" rows=\"3\" name=\"themaReason" + themen.length +"\"></textarea></div></div>");
 		}
 	}
 }
