183992f78136d15fdcd86cff6c57f22b1348d024
lukasepple <post@lukasepple.de>
2013-09-08 11:08:09 +0200

    bug fix

--- system/vote.php
+++ system/vote.php
@@ -8,7 +8,7 @@ $db = new Database(SQL_HOSTNAME, SQL_DATABASE, SQL_USERNAME, SQL_PASSWORD);
 $count = file_get_contents("http://jw.schaub.it/get.php?table=Themengebiete?action=count");
 for($i = 0; $i < $count; $i++){
 	if(isset($_POST['parteiRadio'], $_POST['themaID'. $i], $POST['themaReason' . $i])){
-		$vote_status = $db->vote($_POST['parteiRadio'], $_POST['themaID'. $i], $POST['themaReason' . $i];
+		$vote_status = $db->vote($_POST['parteiRadio'], $_POST['themaID'. $i], $POST['themaReason' . $i]);
 		if($vote_status == False){
 			echo("SQL Error.");
 		}
